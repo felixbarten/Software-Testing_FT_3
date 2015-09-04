4b1522fc7800ac94f6be2922b4ba7352ffed201b
Felix Barten <felixbarten@gmail.com>
2015-09-04 14:22:29 +0200

    cleaned up euler, sped up exercise 9, fixed some incorrect method names

--- src/W1_Euler_bonus/Euler.hs
+++ src/W1_Euler_bonus/Euler.hs
@@ -5,12 +5,17 @@ import Data.List
 
 --- Project Euler exercise 9
 
+{- 
+first (SLOW) solution: 
 pythagoreanTriplet :: [(Integer, Integer, Integer)]
-pythagoreanTriplet = [(a,  b,  c) | a <- [1..500], b <- [2..500], c <- [1..1000], a + b + c == 1000, a^2 + b^2 == c^2, a<b, b<c]
--- ^ will run for a loong time
+pythagoreanTriplet = [(a,  b,  c) | a <- [1..500], b <- [2..500], c <- [1..1000], a + b + c == 1000, a^2 + b^2 == c^2, a<b && b<c]
+-}
 
-productOf :: Integer
-productOf = foldr (\(x,y,z) acc -> product [x,y,z] ) 0 pythagoreanTriplet
+pythagoreanTriplet :: [(Integer, Integer, Integer)]
+pythagoreanTriplet =  [(a,  b,  c) | j <- [2..500], i <- [1..(j-1)], let a = i, let b = j, let c = j + i, a + b + c == 1000, a^2 + b^2 == c^2, a<b && b<c]
+
+euler_problem_9 :: Integer
+euler_problem_9 = foldr (\(x,y,z) _ -> product [x,y,z] ) 0 pythagoreanTriplet
 
 --- Project Euler exercise 10
 
@@ -21,8 +26,8 @@ prime n = n > 1 && all (\ x -> rem n x /= 0) xs
 primes :: [Integer]
 primes = filter prime [2..]
 
-sumOfPrimes ::  Integer
-sumOfPrimes = sum (takeWhile (< 2000000) primes)
+euler_problem_10 ::  Integer
+euler_problem_10 = sum (takeWhile (< 2000000) primes)
 
 -- Project Euler exercise 49
 
@@ -30,15 +35,16 @@ fourDigitPrimes :: [Integer]
 fourDigitPrimes = dropWhile (<=1488) (takeWhile (<10000) primes)
 -- ^ set lowerbound to 1488 to prevent returning (1487,4817,8147)
 
-getPermutations :: [Integer] -> [Integer] -> (Integer,Integer,Integer)
-getPermutations [] _ = (0,0,0)
-getPermutations (x:xs) ys = if result == (0,0,0) then getPermutations xs ys else result 
-    where result =  permutation x ys 
+getPrimeTriples :: [Integer] -> [Integer] -> (Integer,Integer,Integer)
+getPrimeTriples [] _ = (0,0,0)
+getPrimeTriples (x:xs) ys = if result == (0,0,0) then getPrimeTriples xs ys else result 
+    where result =  getPrimeTriple x ys 
     
-permutation :: Integer -> [Integer] -> (Integer, Integer, Integer)
-permutation _ [] = (0,0,0)
-permutation x (y:ys) = if x /= y && prime (y + (y - x)) && isPermutation x y (y + y - x)
-  then (x, y, y + y - x) else permutation x ys
+getPrimeTriple :: Integer -> [Integer] -> (Integer, Integer, Integer)
+getPrimeTriple _ [] = (0,0,0)
+getPrimeTriple x (y:ys) = if x /= y && prime z && isPermutation x y z
+  then (x, y, z) else getPrimeTriple x ys
+  where z = y + (y-x)
 
 intToList :: Integer -> [Integer]
 intToList = map (read . return) . show
@@ -46,7 +52,7 @@ intToList = map (read . return) . show
 {-
 isPermutation :: Integer -> Integer -> Integer -> Bool
 isPermutation x y z = (sort ( intToList x) == sort  (intToList y)) && (sort ( intToList y) == sort (intToList z)) && (sort ( intToList x) == sort ( intToList z))
--- ^ dirty implementation
+-- ^ without permutations from data.list
 -}
 
 isPermutation :: Integer -> Integer -> Integer -> Bool
@@ -60,8 +66,5 @@ joinints = read . concatMap show
 concatInts :: (Integer, Integer, Integer) -> Integer
 concatInts (x, y, z) = joinints [x,y,z]
 
-getPrimePermutations :: (Integer, Integer, Integer)
-getPrimePermutations = getPermutations fourDigitPrimes fourDigitPrimes
-
-getConcatPermutation :: Integer
-getConcatPermutation = concatInts getPrimePermutations
+euler_problem_49 :: Integer
+euler_problem_49 = concatInts $ getPrimeTriples fourDigitPrimes fourDigitPrimes
