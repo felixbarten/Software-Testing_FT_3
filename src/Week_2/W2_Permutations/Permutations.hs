746a0954728f0be96091759f799dc82aa7937f5d
Felix Barten <felixbarten@gmail.com>
2015-09-07 16:46:13 +0200

    added permutations (no tests)

--- src/Week_2/W2_Permutations/Permutations.hs
+++ src/Week_2/W2_Permutations/Permutations.hs
@@ -1,2 +1,33 @@
 module Week_2.W2_Permutations.Permutations where
 
+import Data.List
+import System.Random
+
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation xs ys = xs `elem` permutations ys && ys `elem` permutations xs
+
+{- 
+If you assume that the input list does not contain duplicates it would be easier to check if the elements are permutations?
+-}
+
+getRandomInt :: Int -> IO Int
+getRandomInt n = getStdRandom (randomR (0,n))
+
+randomFlip :: Int -> IO Int
+randomFlip x = do 
+   b <- getRandomInt 1
+   if b == 0 then return x else return (-x)
+
+genIntList :: IO [Int]
+genIntList = do 
+  k <- getRandomInt 20
+  n <- getRandomInt 10
+  getIntL k n
+
+getIntL :: Int -> Int -> IO [Int]
+getIntL _ 0 = return []
+getIntL k n = do 
+   x <-  getRandomInt k
+   y <- randomFlip x
+   xs <- getIntL k (n-1)
+   return (y:xs)
