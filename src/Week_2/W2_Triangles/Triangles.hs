bce01d2e0033106006ea8bd252f3ffb7d1806410
Spiros Tzavaras <spirostza77@hotmail.com>
2015-09-07 13:44:30 +0200

    'Triangle exercise'

--- src/Week_2/W2_Triangles/Triangles.hs
+++ src/Week_2/W2_Triangles/Triangles.hs
@@ -1,2 +1,12 @@
 module Week_2.W2_Triangles.Triangles where
 
+data Shape = NoTriangle | Equilateral
+		| Isosceles |Rectangular | Other deriving(Eq,Show)
+
+
+triangle:: Integer -> Integer -> Integer -> Shape
+triangle x y z | x == y && y == z = Equilateral
+		| x == y || x == z || y == z = Isosceles
+		| x*x + y*y == z*z || x*x + z*z == y*y || y*y + z*z == x*x = Rectangular
+		| x+y < z || x+z < y || y+z < x = NoTriangle
+		| otherwise = Other
