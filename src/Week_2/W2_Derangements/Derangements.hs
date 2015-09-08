5e247a4fccb6c43eddce714fdb7287a24c8402fe
Felix Barten <felixbarten@gmail.com>
2015-09-08 15:39:29 +0200

    deran definition

--- src/Week_2/W2_Derangements/Derangements.hs
+++ src/Week_2/W2_Derangements/Derangements.hs
@@ -1,2 +1,30 @@
 module Week_2.W2_Derangements.Derangements where
 
+import Data.List
+import System.Random
+import Week_2.Testing
+
+isDerangement :: Eq a => [a] -> [a] -> Bool
+isDerangement xs ys = deran xs == deran ys
+
+deran :: Eq a => [a] -> [[a]]
+deran xs =  filter (and . zipWith (/=) xs) $ permutations xs
+
+{-
+blah :: [a] -> [[a]]
+blah xs = deran_own permutations xs
+-}
+{-
+deran_own :: Eq a => [a] -> [[a]]
+deran_own [] = []
+deran_own (x:xs) =  if x /= head (head ls) then [x] : deran_own xs else deran_own xs
+    where ls = permutations (x:xs)
+   -}
+
+deran_own :: [a] -> [[a]]
+deran_own [] = [[]]
+deran_own (x:xs) =  do 
+                    ls <- permutations xs 
+                    if x /= (head (head ls)) then [x] : deran_own xs else deran_own xs
+                    
+                    
