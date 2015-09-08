1054dcf88b944c070720c2860daae89653e95476
Spiros Tzavaras <spirostza77@hotmail.com>
2015-09-08 23:31:38 +0200

    task_3 completed...maybe to be beautified...

--- src/Week_2/W2_Derangements/Derangements.hs
+++ src/Week_2/W2_Derangements/Derangements.hs
@@ -1,42 +1,29 @@
 module Week_2.W2_Derangements.Derangements where
 
---isDerangement:: Eq a => [a] -> [a] -> Bool
 isDerangement:: [Int] -> [Int] -> Bool
-isDerangement xs ys = checkItemDerangement xs ys 0
+isDerangement xs ys = checkItemDerangement xs xs ys
+
+checkItemDerangement:: [Int] -> [Int] -> [Int] -> Bool
+checkItemDerangement _ [] [] = True
+checkItemDerangement _ _ [] = False
+checkItemDerangement [] [] _ = False
+checkItemDerangement xs zs ys =
+		if ((head ys) `elem` xs) && (head ys /= head zs)
+		then checkItemDerangement xs (removeFromList (head zs) zs) (removeFromList (head ys) ys)
+		else False
+
+{- *** below wrong version ***
 
 checkItemDerangement:: [Int] -> [Int] -> Int -> Bool
 checkItemDerangement [] [] position = True
 checkItemDerangement _ [] position = False
 checkItemDerangement [] _ position = False
 checkItemDerangement xs ys position =
-		--if (position == (length ys) - 1)
-		--then if (xs == ys && (head ys) /= position) -- ys!!position
-		--	then True
-		--	else False
 		if (((head ys) `elem` xs) && ((head ys) /= position)) --
 		then checkItemDerangement (removeFromList (head ys) xs) (removeFromList (head ys) ys) (position+1)
 		else False
 
---isDerangement [] [] = True
---isDerangement _ [] = False
---isDerangement [] _ = False
---isDerangement xs ys = (isPermutation xs ys) && [(y!!n /= n) | y<- ys, n<-[0..length[ys]-1]]
---isDerangement xs (y:ys)
---		| y `elem` xs
---		if (isPermutation xs ys)
---		then validateDerangement ys 0
---		else False
-
-validateDerangement:: Eq a => [a] ->  a -> Bool
-validateDerangement n l = if n == []
-				then True
-				else False
-
---validateDerangement [] position = True
---validateDerangement (y:ys) position = ((y!!position) /= position) && (validateDerangement ys (position+1))
-			--if (y!!startList /= startList)
-			--then validateDerangement ys startList+1
-			--else False
+-}
 
 
 removeFromList:: Eq a => a -> [a] -> [a]
