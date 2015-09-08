9ff7af5bc336c9ebe7178616d73b1f242d31614b
Spiros Tzavaras <spirostza77@hotmail.com>
2015-09-08 02:01:47 +0200

    permutations

--- src/Week_2/W2_Permutations/Permutations.hs
+++ src/Week_2/W2_Permutations/Permutations.hs
@@ -1,2 +1,18 @@
 module Week_2.W2_Permutations.Permutations where
 
+-- is the 2nd one permutation of 1st one?
+
+isPermutation:: Eq a => [a] -> [a] -> Bool
+isPermutation [] [] = True
+isPermutation _ [] = False
+isPermutation [] _ = False
+isPermutation xs (y:ys)
+		| y `elem` xs = (isPermutation (removeFromList y xs) ys)
+		| otherwise = False
+
+
+removeFromList:: Eq a => a -> [a] -> [a]
+removeFromList _ [] = []
+removeFromList x (y:ys) 
+		| x == y = ys
+		| otherwise = y: removeFromList x ys
