a80f49c0ccbac13eeddebd34f452845e87dea113
Spiros Tzavaras <spirostza77@hotmail.com>
2015-09-14 00:53:48 +0200

    euler 6

--- src/Week_2/W2_Euler_bonus/Euler.hs
+++ src/Week_2/W2_Euler_bonus/Euler.hs
@@ -7,3 +7,16 @@ calcSumOfMultiOfThreeOrFive = sum . takeWhile (<1000) . filter divi $ [1..]
 
 divi :: Integer -> Bool
 divi x = x `mod` 5 == 0 || x `mod` 3 == 0
+
+-- euler 6
+
+sumOfSquares :: Integer  -> Integer
+sumOfSquares x = sum (map (^2) [1..x])
+
+squareOfSums:: Integer -> Integer
+squareOfSums x = (sum [1..x]) ^2
+
+calculateDifference :: Integer -> Integer
+calculateDifference x =  squareOfSums x - sumOfSquares x
+
+-- result: 25164150
