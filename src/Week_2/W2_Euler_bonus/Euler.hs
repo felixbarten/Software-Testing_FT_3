05a8453f298b406b552e42fc88ec1c088cb117df
Spiros Tzavaras <spirostza77@hotmail.com>
2015-09-13 18:40:44 +0200

    euler 1

--- src/Week_2/W2_Euler_bonus/Euler.hs
+++ src/Week_2/W2_Euler_bonus/Euler.hs
@@ -1,2 +1,9 @@
 module Week_2.W2_Euler_bonus.Euler where
 
+-- euler 1
+
+calcSumOfMultiOfThreeOrFive :: Integer  
+calcSumOfMultiOfThreeOrFive = sum . takeWhile (<1000) . filter divi $ [1..]
+
+divi :: Integer -> Bool
+divi x = x `mod` 5 == 0 || x `mod` 3 == 0
